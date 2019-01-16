f23af276bdfcfd9329c7cee2de65df01608e02ae
FLiunxC <1142709791@qq.com>
2019-01-16 11:46:32 +0800

    重新优化了pushButton的订制
    可以实现左图中文字右图的三种变化的button

--- qtCustomWidget/customWidget/PushButton/PushButton.cpp
+++ qtCustomWidget/customWidget/PushButton/PushButton.cpp
@@ -1,31 +1,87 @@
 #include "PushButton.h"
 #include <QPainter>
 #include <QString>
+#include <QDebug>
+#include <QApplication>
+#include <QSizePolicy>
+#include <QSpacerItem>
+
 
 PushButton::PushButton(QWidget *parent) : QPushButton(parent)
 {
+    initUILayout();
+}
+
+void PushButton::setButtonStyleSheet()
+{
+    this->setObjectName("pButtonOk");
+
+    QString styleSheetQSS = "";
+
+    //如果启动悬浮模式则使用悬浮qss
+    if(m_hoverEnabled)
+    {
+        styleSheetQSS = QString("QPushButton{outline:0px;border:0px;}" \
+                                "QPushButton#pButtonOk  \
+                                {   background-color:%1; \
+                                    border: %4 solid %5; \
+                                } \
+                                QPushButton#pButtonOk:hover \
+                                { \
+                                    color:%2;   \
+                                    background-color:%3; \
+                                    border: %4 solid %5; \
+                                } \
+                                ").arg(m_backgroundColor, m_hoverFontColor, m_hoverBgColor, m_borderWide, m_borderColor);
+    }
+    else
+    {
+        qInfo()<<"进来----";
+        styleSheetQSS = QString("QPushButton#pButtonOk  \
+        { \
+                                    background-color:%1; \
+                                    border-radius:2px;  \
+                                    border:%2 solid %3;}").arg(m_backgroundColor, m_borderWide, m_borderColor);
+    }
+
+    this->setStyleSheet(styleSheetQSS);
 
 }
 
-void PushButton::initDefaultStyleSheet()
+void PushButton::initUILayout()
 {
-    this->setObjectName("pButtonOk");
-    this->setStyleSheet(QString("QPushButton{outline:0px;"
-                        "border:0px;"
-                        "}"
-                        "QPushButton#pButtonOk  \
-                        { \
-                            color:white; \
-                            background-color:%1; \
-                            border: 1px solid rgb(11 , 137 , 234); \
-                        } \
-                        QPushButton#pButtonOk:hover \
-                        { \
-                            color:white;   \
-                            background-color:rgb(44 , 137 , 255); \
-                            border: 1px solid rgb(11 , 137 , 234); \
-                        } \
-                        ").arg(m_backgroundColor));
+
+    QSizePolicy  sizePolicy(QSizePolicy::Preferred,QSizePolicy::Preferred);
+    sizePolicy.setHorizontalStretch(0);
+    sizePolicy.setVerticalStretch(0);
+    sizePolicy.setHeightForWidth(this->sizePolicy().hasHeightForWidth());
+    this->setSizePolicy(sizePolicy);
+
+    m_horizontalLayout = new QHBoxLayout(this);
+     //加弹簧
+    m_HspacerItemStart = new QSpacerItem(20,20, QSizePolicy::Expanding);
+    m_horizontalLayout->addSpacerItem(m_HspacerItemStart);
+
+//    horizontalLayout->setStretch(0, 1);
+//    horizontalLayout->setStretch(1, 4);
+    //加弹簧
+    m_HspacerItemEnd = new QSpacerItem(20,20, QSizePolicy::Expanding);
+    m_horizontalLayout->addSpacerItem(m_HspacerItemEnd);
+
+    m_horizontalLayout->setContentsMargins(0, 0, 0,0);
+    this->setText("");
+
+}
+
+void PushButton::setButtonMaxAndMinValue(QWidget *widget , QSize size)
+{
+    //设置最大值
+    widget->setMaximumWidth(size.width());
+    widget->setMaximumHeight(size.height());
+
+    //设置最小值
+    widget->setMinimumWidth(size.width());
+    widget->setMinimumHeight(size.height());
 }
 
 void PushButton::setClickEffect(bool clickEffect)
@@ -33,22 +89,22 @@ void PushButton::setClickEffect(bool clickEffect)
     m_isClickEffect = clickEffect;
 }
 
-void PushButton::setBackgroundImage(QString imgPath)
+void PushButton::setBackgroundImage(QString imgPath, QSize size)
 {
     m_backGroundImage = imgPath;
     this->setIcon(QIcon(QPixmap(m_backGroundImage)));
-    this->setIconSize(this->size());
+    this->setIconSize(size);
 }
 
 void PushButton::setButtonTransparency(bool isTransparency)
 {
-     this->setStyleSheet("QPushButton \
+    this->setStyleSheet("QPushButton \
     { \
-        color:white; \
-        background-color:#00000000; \
-        border: 0px solid rgb(11 , 137 , 234); \
-    } "
-     );
+                            color:white; \
+                            background-color:#00000000; \
+                            border: 0px solid rgb(11 , 137 , 234); \
+                        } "
+                        );
 }
 
 
@@ -57,7 +113,7 @@ void PushButton::mousePressEvent(QMouseEvent *e)
     if(m_isClickEffect){
         pressClickStyleSheet();
     }
-     e->accept();
+    e->accept();
 }
 
 void PushButton::mouseReleaseEvent(QMouseEvent *e)
@@ -65,7 +121,7 @@ void PushButton::mouseReleaseEvent(QMouseEvent *e)
     sigClick();
     sigClick(m_isClickSecond);
     if(m_isClickEffect){
-         releaseClickStyleSheet();
+        releaseClickStyleSheet();
     }
 
     m_isClickSecond = !m_isClickSecond;
@@ -73,29 +129,41 @@ void PushButton::mouseReleaseEvent(QMouseEvent *e)
     e->accept();
 }
 
+void PushButton::mouseMoveEvent(QMouseEvent *e)
+{
+    e->accept();
+}
+
 void PushButton::pressClickStyleSheet()
 {
-
-    this->setStyleSheet("  QPushButton#pButtonOk  \
+    /*
+    this->setStyleSheet(QString("#pButtonOk  \
     {  \
-        color:white;  \
-        background-color:rgb(14 , 135 , 228); \
-        border: 1px solid rgb(12 , 138 , 235);  \
-        padding-left:3px; \
-        padding-top:3px; \
-    }");
+                                    color:%1;  \
+                                    border: %3 solid %4;  \
+                                    padding-left:3px; \
+                                    padding-top:3px; \
+                                }").arg(m_fontColor, m_borderWide, m_borderColor));
+                                */
+
+    m_horizontalLayout->setContentsMargins(4, 3, 0, 0);
+
 }
 
 void PushButton::releaseClickStyleSheet()
 {
-    this->setStyleSheet("QPushButton#pButtonOk  \
+
+   m_horizontalLayout->setContentsMargins(0, 0, 0, 0);
+    /*
+    this->setStyleSheet(QString("#buttonText#button  \
     {  \
-        color:white;  \
-        background-color:rgb(14 , 135 , 228); \
-        border: 1px solid rgb(12 , 138 , 235);  \
-        padding-left:0px; \
-        padding-top:0px; \
-                        }");
+                                    color:%1;  \
+                                    border: %3 solid %4;  \
+                                    padding-left:0px; \
+                                    padding-top:0px; \
+                                }").arg(m_fontColor, m_borderWide, m_borderColor));
+                                */
+
 }
 
 void PushButton::setBackgroundColor(QString backgroundColor)
@@ -103,8 +171,107 @@ void PushButton::setBackgroundColor(QString backgroundColor)
     if (m_backgroundColor == backgroundColor)
         return;
 
-    m_backgroundColor = backgroundColor;
-    initDefaultStyleSheet();
+    if(!backgroundColor.isEmpty())
+    {
+        m_backgroundColor = backgroundColor;
+    }
+
+    setButtonStyleSheet();
+}
+
+void PushButton::setFontColor(QString fontColor)
+{
+        m_fontColor = fontColor;
+        m_buttonText->setStyleSheet(QString("#buttonText{color:%1}").arg(m_fontColor));
+}
+
+void PushButton::setButtonText(const QString &text)
+{
+     m_horizontalLayout->removeItem(m_HspacerItemEnd);
+    //初始化ui
+    if(m_buttonText == nullptr)
+    {
+        m_buttonText = new QLabel(this);
+        m_buttonText->setObjectName("buttonText");
+        m_buttonText->setText(this->text());
+        this->setText("");
+    }
+
+    m_buttonText->setText(text);
+    m_horizontalLayout->insertWidget(2, m_buttonText);
+    m_horizontalLayout->addSpacerItem(m_HspacerItemEnd);
+}
+
+void PushButton::setText(const QString &text)
+{
+    setButtonText(text);
+}
+
+void PushButton::setLeftIcon(const QString &Licon, const QSize & size)
+{
+     m_horizontalLayout->removeItem(m_HspacerItemEnd);
+    if(m_buttonLeftIcon == nullptr)
+    {
+        m_buttonLeftIcon = new QLabel(this);
+        m_buttonLeftIcon->setObjectName("buttonLIcon");
+        m_buttonLeftIcon->setScaledContents(true);
+    }
+
+    if(!size.isEmpty())
+    {
+        setButtonMaxAndMinValue(m_buttonLeftIcon, size);
+    }
+    m_leftIconSource = Licon;
+
+    m_buttonLeftIcon->setStyleSheet("#buttonLIcon { background-position:center; background-repeat:no_repeat; image: url("+Licon+");}");
+
+    m_horizontalLayout->insertWidget(1, m_buttonLeftIcon);
+    m_horizontalLayout->addSpacerItem(m_HspacerItemEnd);
+}
+
+void PushButton::setRightIcon(const QString &Ricon, const QSize &size)
+{
+    m_horizontalLayout->removeItem(m_HspacerItemEnd);
+    if(m_buttonRightIcon == nullptr)
+    {
+        m_buttonRightIcon = new QLabel(this);
+        m_buttonRightIcon->setObjectName("buttonRIcon");
+        m_buttonRightIcon->setScaledContents(true);
+    }
+    else
+    {
+        m_horizontalLayout->removeWidget(m_buttonRightIcon);
+    }
+
+    if(!size.isEmpty())
+    {
+         setButtonMaxAndMinValue(m_buttonRightIcon, size);
+    }
+
+    m_buttonRightIcon->setStyleSheet("");
+    m_buttonRightIcon->setStyleSheet("#buttonRIcon {image: url("+Ricon+");}");
+
+   m_buttonRightIcon->setStyle(QApplication::style());
+
+    m_horizontalLayout->insertWidget(3, m_buttonRightIcon);
+    m_horizontalLayout->addSpacerItem(m_HspacerItemEnd);
+}
+
+void PushButton::setHoverEnabled(bool Enabled)
+{
+    m_hoverEnabled = Enabled;
+}
+
+void PushButton::setHoverColor(const QString &hoverBgColor, const QString &hoverFontColor)
+{
+    m_hoverBgColor = hoverBgColor;
+    m_hoverFontColor = hoverFontColor;
+}
+
+void PushButton::setButtonBorderWideAndColor(const QString &borderWide, const QString &borderColor)
+{
+    m_borderColor = borderColor;
+    m_borderWide = borderWide;
 }
 
 void PushButton::enterEvent(QEvent *event)
--- qtCustomWidget/customWidget/PushButton/PushButton.h
+++ qtCustomWidget/customWidget/PushButton/PushButton.h
@@ -1,8 +1,19 @@
 #ifndef PUSHBUTTON_H
 #define PUSHBUTTON_H
 
+/**
+* @brief: QPushButton的自定义button控件
+* @author: Fu_Lin
+* @date:
+* @description:左icon，中文本，右icon， 可多次点击
+*/
+
+
+
 #include <QPushButton>
 #include <QMouseEvent>
+#include <QLabel>
+#include <QHBoxLayout>
 
 class PushButton : public QPushButton
 {
@@ -11,17 +22,50 @@ class PushButton : public QPushButton
 public:
     explicit PushButton(QWidget *parent = nullptr);
 
-
-    //初始化默认样式
-    void initDefaultStyleSheet();
     //设置是否有点击效果
     void setClickEffect(bool clickEffect);
     //设置背景图片
-    void setBackgroundImage(QString imgPath);
+    void setBackgroundImage(QString imgPath, QSize size = QSize());
     //设置背景是否透明，不影响图片的设置，单纯的背景透明
     void setButtonTransparency(bool isTransparency);
     //设置背景颜色，默认颜色是蓝色
     void setBackgroundColor(QString backgroundColor);
+    //设置字体颜色， 默认字体颜色是灰色
+    void setFontColor(QString fontColor);
+    //设置按钮文本
+    void setButtonText(const QString &text);
+    void setText(const QString &text);
+    //设置按钮左边的图标
+    void setLeftIcon(const QString &Licon, const QSize &size = QSize(0, 0));
+    //设置按钮右边的图标
+    void setRightIcon(const QString &Ricon, const QSize &size = QSize(0, 0));
+    /**
+     * @brief setHoverEnabled 是否启动鼠标悬浮改变按钮颜色
+     * @param Enabled treu启动，false禁止(默认是false)
+     */
+    void setHoverEnabled(bool Enabled);
+
+    /**
+     * @brief setHoverColor 设置鼠标悬浮时的按钮背景颜色和字体颜色
+     * @param hoverBgColor  悬浮按钮背景颜色
+     * @param hoverFontColor 悬浮按钮字体颜色
+     */
+    void setHoverColor(const QString &hoverBgColor, const QString & hoverFontColor);
+
+    /**
+     * @brief setButtonBorderWideAndColor 设置按钮的边框线的宽度值和边框颜色
+     * @param borderWide
+     * @param borderColor
+     */
+    void setButtonBorderWideAndColor(const QString &borderWide, const QString &borderColor);
+private:
+    //初始化默认样式
+    void setButtonStyleSheet();
+    //初始化ui布局，左icon，中文本，右icon
+    void initUILayout();
+
+    //设置按钮最大值和最小值
+    void setButtonMaxAndMinValue(QWidget * widget, QSize size);
 
 protected:
 
@@ -29,7 +73,7 @@ protected:
     void leaveEvent(QEvent *event);
     void mousePressEvent(QMouseEvent *e);
     void mouseReleaseEvent(QMouseEvent *e);
-
+    void mouseMoveEvent(QMouseEvent *e);
 private:
     void pressClickStyleSheet();
     void releaseClickStyleSheet();
@@ -38,6 +82,7 @@ signals:
     void sigClick();
     //该信号可以监听点击按钮的次数，isSecondClick ---false为第一次点击，true为第二次点击，只能交替监听2次点击
     void sigClick(bool isSecondClick);
+    //背景颜色改变信号
     void backgroundColorChanged(QString backgroundColor);
 
 public slots:
@@ -46,11 +91,29 @@ private slots:
 
 
 private:
-    bool m_isClickSecond = false;
-    bool m_isClickEffect = false;
-    QString m_backGroundImage;
-    QString m_backgroundColor = "rgb(14 , 150 , 254)";
+    bool m_isClickSecond = false; //是否启动二次点击，启动后每点击一下触发一次信号
+    bool m_isClickEffect = false;  //是否启动点击效果
 
+    QLabel * m_buttonText = nullptr; //按钮文本
+    QLabel * m_buttonLeftIcon = nullptr; //左边按钮图片
+    QLabel * m_buttonRightIcon = nullptr; //右边按钮图片
+
+    QString m_backGroundImage; //按钮的icon图标
+    QString m_backgroundColor; //按钮正常背景颜色
+    QString m_fontColor = "#656565";  //按钮正常字体颜色
+    QString m_hoverBgColor;  //悬浮背景颜色
+    QString m_hoverFontColor;  //悬浮字体颜色
+    QString m_borderWide = "1px"; //边框线宽度
+    QString m_borderColor = "#DEDEDE"; //边框线颜色
+    QString m_leftIconSource = "" ; //左边图标的链接
+    QString m_rightIconSource = "" ; //右边图标的链接
+
+    bool m_hoverEnabled  = false; //是否使能鼠标悬浮，默认时false
+
+    //按钮布局三神器，提供动态布局伸缩
+    QHBoxLayout * m_horizontalLayout = nullptr;
+    QSpacerItem * m_HspacerItemStart = nullptr;
+    QSpacerItem * m_HspacerItemEnd = nullptr;
 };
 
 #endif // PUSHBUTTON_H
